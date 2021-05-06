e78c17adf684ce3648494a47486db3f94341a7bb
zim32 <Yurij.Uvarov@gmail.com>
2021-05-06 22:12:16 +0300

    Fix ValidationException

--- src/Exception/Api/ValidationException.php
+++ src/Exception/Api/ValidationException.php
@@ -13,20 +13,24 @@ class ValidationException extends BaseApiException
 
     /**
      * ValidationException constructor.
-     * @param ConstraintViolationListInterface|ConstraintViolation[] $errors
+     * @param ConstraintViolationListInterface|ConstraintViolation[]|array $errors
      * @param \Throwable|null $previous
      * @param array $headers
      */
-    public function __construct(ConstraintViolationListInterface $errors, \Throwable $previous = null, array $headers = [])
+    public function __construct($errors, \Throwable $previous = null, array $headers = [])
     {
         $payload = [];
 
-        foreach ($errors as $error) {
-            if (!array_key_exists($error->getPropertyPath(), $payload)) {
-                $payload = [];
-            }
+        if ($errors instanceof ConstraintViolationListInterface) {
+            foreach ($errors as $error) {
+                if (!array_key_exists($error->getPropertyPath(), $payload)) {
+                    $payload[$error->getPropertyPath()] = [];
+                }
 
-            $payload[$error->getPropertyPath()][] = $error->getMessage();
+                $payload[$error->getPropertyPath()][] = $error->getMessage();
+            }
+        } else {
+            $payload = $errors;
         }
 
         parent::__construct($payload, $previous, $headers);
